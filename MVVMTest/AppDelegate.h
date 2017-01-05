667383562ac8ed26aa26f6767f8d6930ab16af26
MintCat <wanghao1136@163.com>
2017-01-05 10:18:00 +0800

    Initial Commit

---
+++ MVVMTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMTest
+//
+//  Created by ios on 2017/1/5.
+//  Copyright © 2017年 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
